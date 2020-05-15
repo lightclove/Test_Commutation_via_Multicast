38d210301053da13bec3a5ec7fc4c889a7ba5221
Dmitry ILiyushkó <79217526217@ya.ru>
2020-05-15 19:01:16 +0300

    Create OS_Info.java

---
+++ OS_Info.java
@@ -0,0 +1,23 @@
+package ru.nppame.utility;
+
+public class OS_Info {
+  public static boolean isWindows() {
+    String os = System.getProperty("os.name").toLowerCase();
+    return (os.indexOf("win") >= 0);
+  }
+  
+  public static boolean isMac() {
+    String os = System.getProperty("os.name").toLowerCase();
+    return (os.indexOf("mac") >= 0);
+  }
+  
+  public static boolean isUnix() {
+    String os = System.getProperty("os.name").toLowerCase();
+    return (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0);
+  }
+  
+  public static String getOSVerion() {
+    String os = System.getProperty("os.version");
+    return os;
+  }
+}
